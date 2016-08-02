1a52f4bb4c5da6451b6daa3bb03b2319504420ed
Andrew Klein <andrew.klein.j@gmail.com>
2016-08-02 15:08:50 -0400

    add more functionality to distance class

--- app/models/distances.rb
+++ app/models/distances.rb
@@ -1,11 +1,14 @@
 require 'pry'
 class Distances
 
-  attr_reader :user
+  attr_reader :user, :subways_with_distances, :sorted_hashes
 
   def initialize(user)
     #user.location will = coordinates
     @user = user
+    @subways_with_distances = gather_distances
+    @sorted_hashes = sort_by_distance(@subways_with_distances)
+    binding.pry
   end
 
   # user.location => coordinates of user as an array of 2 elements
@@ -31,8 +34,23 @@ class Distances
   def gather_distances
     @subway_data = SubwayStation.all
     @subway_data.collect do |subway|
-      calculate_distance(subway.coordinates, user.coordinates)
+      distance = calculate_distance(subway.coordinates, self.user.coordinates)
+      {station: subway, distance: distance}
     end
   end
 
+  def sort_by_distance(subways_distances_array)
+    subways_distances_array.sort_by do |subway_and_distance|
+      subway_and_distance[:distance]
+    end
+  end
+
+end
+
+class User
+  attr_accessor :coordinates
+
+  def initialize
+    @coordinates = [-73.9752089996586,40.680699332136946]
+  end
 end
--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,7 @@
 require_relative '../config/environment'
 
 SubwayAPICaller.new("https://data.cityofnewyork.us/resource/kk4q-3rt2.json").call
-new_distances = Distances.new
+andrew = User.new
+new_distances = Distances.new(andrew)
 binding.pry
 #new_call = SubwayAPICaller.new("https://data.cityofnewyork.us/resource/kk4q-3rt2.json").call
